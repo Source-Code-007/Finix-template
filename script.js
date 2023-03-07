b1e54e96fec8b89d3f08a5949dbc6c474fd6681d
The Soul Investigator <utsho926@gmail.com>
2023-03-07 18:07:09 +0600

    final retouch

--- script.js
+++ script.js
@@ -1,52 +1,33 @@
 // Form validation ---
-function formValidate(){
+function formValidate() {
     let typeTheMoney = document.forms['bLoanForm']['typeTheMoney']
     let borrowing = document.forms['bLoanForm']['borrowing']
     let termOfUse = document.forms['bLoanForm']['termOfUse']
 
     let typeTheMoneyP = document.getElementById('typeTheMoneyP')
-    let borrowingP = document.getElementById('borrowingP') 
-    
-    let alertText
+    let borrowingP = document.getElementById('borrowingP')
 
     // type the money validation
-{ 
-       if(typeTheMoney.value<1000){
-                if(typeTheMoney.value==''){
-                    alertText = '"This field is required" (You can\'t leave this field blank).'
-                    typeTheMoney.style.background = 'rgba(255, 99, 71, 0.291)'
-                    typeTheMoneyP.innerText = alertText
-                    typeTheMoneyP.style.color = 'red'
-                    return false
-                }
-         
-                else if(!typeTheMoney.value == '' && typeTheMoney.value<1000){
-                    alertText = 'Please enter more than 1000$'
-                typeTheMoney.style.background = 'rgba(255, 99, 71, 0.291)'
-                typeTheMoneyP.innerText = alertText
-                typeTheMoneyP.style.color = 'red'
-                return false 
-                } 
-    }
-    if(typeTheMoney.value>1000){
-        typeTheMoneyP.innerText = ''
+    if (typeTheMoney.value >= 1000 && typeTheMoney.value <= 3000){
+        typeTheMoneyP.innerText = 'yess'
+        typeTheMoneyP.style.color = 'green'
         typeTheMoney.style.background = '#5cb853'
+    } else{
+        typeTheMoneyP.innerText = '*Please input valid ammount'
+        typeTheMoneyP.style.color = '#e73c3e'
+        typeTheMoney.style.background = '#e73c3e'
     }
-}
 
-    //borrowing validation
-    if(borrowing.value<10000){
-        if(borrowing.value == ''){
-                alertText = '"This field is required" (You can\'t leave this field blank).'
-                borrowing.style.background = 'rgba(255, 99, 71, 0.291)'
-                borrowingP.innerText = alertText
-                borrowingP.style.color = 'red'
-                return false
-            }
-             alertText = 'Please enter more than 10000$'
-             borrowing.style.background = 'rgba(255, 99, 71, 0.291)'
-             borrowingP.innerText = alertText
-             borrowingP.style.color = 'red'
-             return false       
+    if(borrowing.value>10000 && borrowing.value<50000) {
+        borrowingP.innerText = 'yess'
+        borrowingP.style.color = 'green'
+        borrowing.style.background = '#5cb853'
+    } else{
+        borrowingP.innerText = '*Please input valid ammount'
+        borrowingP.style.color = '#e73c3e'
+        borrowing.style.background = '#e73c3e'
     }
+
+    
+    return false
 }
